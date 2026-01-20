54767e5934e492e54c5909e5fd937305b9321790
Pragat Pandya <pragat.pandya@gmail.com>
2026-01-20 14:00:04 +0530

    Added .gitignore.

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import doten
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0ws8sv@bvmc2ur&6_&srb5o)w7rn49ba770ec%1tnod#9n_7_p'
+SECRET_KEY = '0ws8sv@bvmc2ur&6_&srb5o)w7rn49ba770ec%1tnod#9n_7_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
