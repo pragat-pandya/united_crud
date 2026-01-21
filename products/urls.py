b91bc96b03754796780e7b0334a80a0e2cd51122
Pragat Pandya <pragat.pandya@gmail.com>
2026-01-21 12:43:08 +0530

    Pagination and filtering.

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListCreateAPIView
+from .views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView
 
 
 urlpatterns = [
-    path('products/', ProductListCreateAPIView.as_view(), name='product-list-create')
+    path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name="product-retrieve-desctroy-update")
 ]
--- products/views.py
+++ products/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.filters import SearchFilter
 # Create your views here.
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['name', 'description']
+
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
